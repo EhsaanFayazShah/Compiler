089dbb458cc9331d7e7211d4b545627a5fb68c5f
Ehsaan <ehsaanfayaz80@gmail.com>
2024-11-01 21:15:19 +0530

    traverser added

--- compiler.js
+++ compiler.js
@@ -168,3 +168,42 @@ function parser(tokens) {
 }
 // ===============================The Traverser===============================
 // Now we will traverse our AST using visitor pattern.
+function traverser(ast, visitor) {
+  function traverseArray(array, parent) {
+    array.forEach((child) => {
+      traverseNode(child, parent);
+    });
+  }
+
+  function traverseNode(node, parent) {
+    let methods = visitor[node.type];
+
+    if (methods && methods.enter) {
+      methods.enter(node, parent);
+    }
+
+    switch (node.type) {
+      case "Program":
+        traverseArray(node.body, node);
+        break;
+
+      case "CallExpression":
+        traverseArray(node.params, node);
+        break;
+
+      case "NumberLiteral":
+      case "StringLiteral":
+        break;
+
+      default:
+        throw new TypeError(node.type);
+    }
+
+    if (methods && methods.exit) {
+      methods.exit(node, parent);
+    }
+  }
+
+  traverseNode(ast, null);
+}
+//===================Transformer===============================
