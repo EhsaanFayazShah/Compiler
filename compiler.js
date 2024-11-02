8456ca095e8ae53fba7e4ef63d09e17ff5334357
Ehsaan <ehsaanfayaz80@gmail.com>
2024-11-02 20:26:23 +0530

    Transformer added

--- compiler.js
+++ compiler.js
@@ -207,3 +207,60 @@ function traverser(ast, visitor) {
   traverseNode(ast, null);
 }
 //===================Transformer===============================
+function transformer(ast) {
+  let newAst = {
+    type: "Program",
+    body: [],
+  };
+
+  ast._context = newAst.body;
+
+  traverser(ast, {
+    NumberLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: "NumberLiteral",
+          value: node.value,
+        });
+      },
+    },
+    // Next we have `StringLiteral`
+    StringLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: "StringLiteral",
+          value: node.value,
+        });
+      },
+    },
+
+    CallExpression: {
+      enter(node, parent) {
+        let expression = {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: node.name,
+          },
+          arguments: [],
+        };
+
+        // Next we're going to define a new context on the original
+        // `CallExpression` node that will reference the `expression`'s arguments
+        // so that we can push arguments.
+        node._context = expression.arguments;
+
+        if (parent.type !== "CallExpression") {
+          expression = {
+            type: "ExpressionStatement",
+            expression: expression,
+          };
+        }
+
+        parent._context.push(expression);
+      },
+    },
+  });
+
+  return newAst;
+}
